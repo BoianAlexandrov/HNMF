78924cbb34d9e45870e7caaee242fda0505ff1d4
Ves <vess13@gmail.com>
2025-11-04 03:17:35 -0700

    parallel script

--- pyHNMFk/run_parallel.sh
+++ pyHNMFk/run_parallel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --partition debug
-#SBATCH --nodes 21
-#SBATCH --ntasks-per-node 2
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node 64
 #SBATCH --time 00:05:00
-#SBATCH --job-name run_diff_separation
+#SBATCH --job-name run_diff_separation2
 #SBATCH --mail-user ves@unm.edu
 #SBATCH --mail-type ALL
 
@@ -13,11 +13,14 @@ module load parallel
 module load miniconda3
 pip install .
 # source activate numpy
-TOTAL_OBS=42
+# TOTAL_OBS=64
 
 # seq 0 $((TOTAL_OBS - 1)) | parallel --jobs $SLURM_NTASKS \
 #     --sshloginfile $SLURM_JOB_NODELIST \
 #     "srun --nodes=1 --ntasks=1 --exclusive python run_task.py {}"
 
-seq 0 $((TOTAL_OBS - 1)) | parallel --jobs $SLURM_NTASKS \
+# seq 0 $((TOTAL_OBS - 1)) | parallel --jobs $SLURM_NTASKS \
+#     "python run_task.py {}"
+
+seq 0 63 | parallel --jobs $SLURM_NTASKS \
     "python run_task.py {}"
